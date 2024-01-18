963022cf87d17d534a02c74d4bba00156d7fea2c
Nicola Genovese <nicol.genovese@gmail.com>
2024-01-18 18:03:46 +0000

    addind prices

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - £12', 'Eggs Benedict - £11', 'Oatmeal - £15', 'Frittata - £16'];
+const mainCourseMenu = ['Steak - £16', 'Pasta - £18', 'Burger - £13', 'Salmon - £20'];
+const dessertMenu = ['Cake - £12', 'Ice Cream - £13', 'Pudding - £15', 'Fruit Salad - £12'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
