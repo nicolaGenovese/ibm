78fd1a723515eaf36d3ccf949da4d40033b6f3ef
Nicola Genovese <nicol.genovese@gmail.com>
2024-01-18 16:26:07 +0000

    implementing user experience

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -1,15 +1,15 @@
-const username = document.getElementById('name').value;
-const age = document.getElementById('age').value;
-const email = document.getElementById('email').value;
-const job = document.getElementById('job').value;
-const designation = document.getElementById('designation').value;
-const productType = document.getElementById('productType').value;
-const feedback = document.getElementById('feedbackText').value;
-const submitButton=document.getElementById('submitBtn');
-
-submitButton.onclick = submitFeedback;
-
+// Get input values inside the submitFeedback function
 function submitFeedback() {
+    const username = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    const email = document.getElementById('email').value;
+    const job = document.getElementById('job').value;
+    const designation = document.getElementById('designation').value;
+    const productType = document.getElementById('productType').value;
+    const feedback = document.getElementById('feedbackText').value;
+    const experince = document.getElementById('experinceText').value;
+  
+    // Display user information
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
     document.getElementById('userEmail').innerHTML = email;
@@ -17,14 +17,20 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
-
+    document.getElementById('userExperince2').innerHTML = experince;
+  
     document.getElementById('userInfo').style.display = 'block';
-    
-    alert('Thank you for your valuable feedback')
-}
-
-document.addEventListener('keydown', function(event) {
+  
+    alert('Thank you for your valuable feedback');
+  }
+  
+  // Event listeners
+  const submitButton = document.getElementById('submitBtn');
+  submitButton.addEventListener('click', submitFeedback);
+  
+  document.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
       submitFeedback();
     }
   });
+  
